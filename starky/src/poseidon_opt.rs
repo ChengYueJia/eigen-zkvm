d9d5baae93aad35ad730b6176552ab44d7fb4c96
Paul Cheng <chengyuejia@foxmail.com>
2023-11-17 22:52:33 +0800

    bugfix

--- starky/src/poseidon_opt.rs
+++ starky/src/poseidon_opt.rs
@@ -4,7 +4,7 @@ pub mod x86_64;
 pub mod general;
 
 #[cfg(target_feature = "avx2")]
-pub use self::x86_64::avx2_poseidon_gl::Poseidon;
+pub use self::x86_64::avx2_poseidon_gl::*;
 
 #[cfg(not(target_feature = "avx2"))]
-pub use self::general::Poseidon;
+pub use self::general::*;
